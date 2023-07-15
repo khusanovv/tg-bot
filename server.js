82a23242f9cd8b4c53a6c57c1c396595fdd55e26
khusanovv <boburshohkhusanov@gmail.com>
2023-07-15 14:50:44 +0500

    First js code

---
+++ server.js
@@ -0,0 +1,28 @@
+import TelegramBot from "node-telegram-bot-api"
+const Api = "1665840542:AAHAIoi8GXucpLyjlF8bwmTcrWJEd-RmS2gnpm"
+// import Promise from "bluebird"
+
+const bot = new TelegramBot(Api)
+
+// const Promise = require('bluebird');
+// Promise.config({
+//   cancellation: true
+// });
+
+bot.onText(/\/start/, (msg) =>{
+    bot.sendMessage(msg.chat.id, "salom" + msg.from.first_name || '', {
+        reply_markup: {
+            keyboard: [
+                [{text: "Mac"}], 
+                [{text: "HP"}, {text: "Acer"}]
+            ],
+            resize_keyboard: true,
+            remove_keyboard: true,
+            one_time_keyboard: true,
+    
+            
+        },
+    })
+
+    bot.sendMessage(msg.chat.id, "salom")
+})
